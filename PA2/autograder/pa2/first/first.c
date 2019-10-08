38dabcf112afce733db55687457a8c8ff07b2b8f
srijag2700 <sg1416@ilab3.cs.rutgers.edu>
2019-10-08 18:16:45 -0400

    Updated first.c

--- PA2/autograder/pa2/first/first.c
+++ PA2/autograder/pa2/first/first.c
@@ -1,3 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int set(int x, int n, int v)
+{
+
+}
+
+int comp(int x, int n)
+{
+
+}
+
+int get (int x, int n)
+{
+
+}
+
 int main(int argc, char* argv[])
 {
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        return 0;
+    }
+
+    int x, n, s;
+    char whatDo[10];
+
+    fscanf(file, "%d", &x);
+
+    while(!feof(file))
+    {
+        fscanf(file, "%s\t%d\t%d\n", whatDo, &n, &s);
+    }
 }

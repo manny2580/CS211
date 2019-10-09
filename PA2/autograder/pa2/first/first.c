ef8141b2376d1ea14a5451f09bb133fada87f100
srijag2700 <sg1416@basic.cs.rutgers.edu>
2019-10-08 22:02:43 -0400

    Added compare function, finished main

--- PA2/autograder/pa2/first/first.c
+++ PA2/autograder/pa2/first/first.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int compare(char* s1, char* s2)
+{
+    while (*s1 && *s1 == *s2)
+    {
+        s1++;
+        s2++;
+    }
+    return *s1 - *s2;
+}
+
 unsigned short set(int x, int n, int v)
 {
 
@@ -33,5 +43,19 @@ int main(int argc, char* argv[])
     while(!feof(file))
     {
         fscanf(file, "%s\t%d\t%d\n", whatDo, &n, &s);
+        if(compare(whatDo, "set") == 0)
+        {
+            set(x, n, s)
+        }
+        else if(compare(whatDo, "comp") == 0)
+        {
+            comp(x, n);
+        }
+        else if (compare(whatDo, "get") == 0)
+        {
+            get(x, n);
+        }
+
+
     }
 }

a1c6fa69bffbbc9c6740ca6182337f9d1217743f
srijag2700 <sg1416@ilab3.cs.rutgers.edu>
2019-11-28 13:38:20 -0500

    Started main method

--- PA4/pa4_autograder/pa4/first/first.c
+++ PA4/pa4_autograder/pa4/first/first.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
+/*
+ * Input: a file listing a circuit description + directives on each line
+ * 
+ * Go through the file with every possible combination of inputs
+ * 
+ * Output: truth table for the circuit description
+ * 
+ * Todo:
+ * Code each of the operations (NOT, AND, OR, NAND, NOR, XOR)
+ * Main method - read file + how to keep track of combinations of inputs
+ * */
+
+
+
 int main(int argc, char* argv[])
 {
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        return 0;
+    }
+    
 }
